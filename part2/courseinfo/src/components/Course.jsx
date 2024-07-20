7c1ae31effe6fd81988757ea864ce39eb5f488a7
sedfarouk <sadikalhanssah@gmail.com>
2024-07-20 22:10:07 +0000

    finish exercise 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,9 @@
-const Course = (props) => {
-    const total_sum = props.course.parts.map(x => x.exercises)
-                                        .reduce((a, b) => a + b, 0)
+const Course = ({course}) => {
+    const total_sum = course.parts.reduce((prev, {exercises}) => prev + exercises, 0)
 
-    const Header = () => <h1>{props.course.name}</h1>
+    const Header = () => <h1>{course.name}</h1>
       
-    const Content = () => <>{props.course.parts.map(x => <p key = {x.id}>{x.name} {x.exercises}</p>)}</>
+    const Content = () => <>{course.parts.map(x => <p key = {x.id}>{x.name} {x.exercises}</p>)}</>
 
     const Total = () => <h5>total of {total_sum} exercises</h5>
 
