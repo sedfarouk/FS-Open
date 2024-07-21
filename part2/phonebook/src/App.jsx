239903c37426ccc717488af1e19b5e2ab7291f09
sedfarouk <sadikalhanssah@gmail.com>
2024-07-21 00:04:52 +0000

    finish exercise 2.7 and 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '1000'
+     }
   ]) 
 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchItem, setSearchItem] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -16,17 +21,35 @@ const App = () => {
       alert(`${input} already added to phonebook`)
 
     else{
-      const nameObject = {name: newName}
+      const nameObject = {
+        name: input,
+        number: newNumber
+      }
+
+      setNewNumber('')
       setPersons(persons.concat(nameObject))
     } 
   }
 
+  const results = persons.filter((x) => {
+      x = x.name.toLowerCase()
+      return x.includes(searchItem)
+    })
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchItem} onChange={(e) => setSearchItem(e.target.value)} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -34,7 +57,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(x => <li key={x.name}>{x.name}</li>)}
+        {results.map(x => <li key={x.name}>{x.name} - {x.number}</li>)}
       </ul>
     </div>
   )
